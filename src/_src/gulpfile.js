a6b87e61c3508ea61a7adacf9726110448b1d157
pyrovers <pyroverse.392@gmail.com>
2015-02-28 17:22:04 +0900

    フロントエンド側の準備

---
+++ src/_src/gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var compass = require('gulp-compass');
+var typescript = require('gulp-typescript');
+var uglify = require('gulp-uglify');
+var plumber = require('gulp-plumber');
+
+gulp.task('typescript', function() {
+	return gulp.src('typescript/*.ts')
+		.pipe(plumber())
+		.pipe(typescript())
+		.pipe(uglify())
+		.pipe(gulp.dest('../js'));
+});
+
+gulp.task('compass', function() {
+	gulp.src('sass/*.scss')
+		.pipe(plumber())
+		.pipe(compass({
+			config_file: './config.rb',
+			css: '../css',
+			sass: 'sass',
+			comments: false
+		}));
+});
+
+gulp.task('watch', ['typescript', 'compass'], function() {
+	gulp.watch('typescript/**', ['typescript']);
+	gulp.watch('sass/**', ['compass']);
+});
